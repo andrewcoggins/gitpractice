15a8179a1d838e45a84af396bfcd681d279c72e0
Andrew Coggins <acoggins38@gmail.com>
2018-01-17 10:05:21 -0500

    some changes

---
+++ src/practice/Prac.java
@@ -0,0 +1,19 @@
+package practice;
+
+/**
+ * this is for practice.
+ * @author andrew
+ *
+ */
+public  class Prac {
+  
+  private int num; 
+  
+  public Prac(int aNum) {
+    this.num = aNum; 
+  }
+  
+  public void printNum() {
+    System.out.println(num);
+  }
+}

b15ce0f8bb5de049b4386fbff2bfc81bc3ec84e1
ksyan <kerryan34@gmail.com>
2018-01-17 10:28:36 -0500

    added pracTwo methods

--- src/practice/PracTwo.java
+++ src/practice/PracTwo.java
@@ -1,5 +1,15 @@
 package practice;
 
 public class PracTwo { 
+  private String word; 
   
+  public PracTwo(String word) {
+    this.word = word; 
+  }
+  
+  //the purpose of this is to print a string. 
+  public void printStr() {
+    System.out.println(word);
+  }
+
 }

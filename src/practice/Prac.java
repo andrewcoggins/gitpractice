205349e30ac5e85f4e4da5302965fbb3540cc5c1
ksyan <kerryan34@gmail.com>
2018-01-17 10:39:59 -0500

    spacing

--- src/practice/Prac.java
+++ src/practice/Prac.java
@@ -10,26 +10,36 @@ public  class Prac {
   private int num; 
   
   public Prac(int aNum) {
+
     this.num = aNum; 
+
   }
   
   //the purpose of this is to print a number. 
   public void printNum() {
+    
     System.out.println(num);
+
   }
   
   // adds 5 to number
   public void addFive() {
+    
     this.num = this.num + 5;
+  
   }
   
   // adds 9 to a number.
   public void addNine() {
+    
     this.num = num + 9; 
+  
   }
   
   // adds 11 to a number
   public void addEleven(){
+    
     this.num = num+11;
+  
   }
 }
